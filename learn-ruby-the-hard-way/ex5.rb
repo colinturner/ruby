dc47ed479fa783fa6091267d88dd264246418ca3
Colin Turner <colinturner92@gmail.com>
2017-02-09 14:38:40 +0700

    ex6

--- learn-ruby-the-hard-way/ex5.rb
+++
@@ -1,4 +0,0 @@
-hilarious = false
-joke_evaluation = "Isn't that joke so funny??? #{hilarious}"
-
-puts joke_evaluation
---
+++ learn-ruby-the-hard-way/ex6.rb
@@ -0,0 +1,9 @@
+hilarious = false
+joke_evaluation = "Isn't that joke so funny??? #{hilarious}"
+
+puts joke_evaluation
+
+w = "Left side of text... "
+e = "and the right side!"
+
+puts w + e
